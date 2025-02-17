f7fa6753bbf50f0dcc711d79426ef42d150c692f
Abderrahhmane Metnani <abdulrahman.metnani@gmail.com>
2025-02-17 10:03:47 +0400

    Lab 4
    Done

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -1,9 +1,9 @@
 fn vec_loop(input: &[i32]) -> Vec<i32> {
     let mut output = Vec::new();
 
-    for element in input {
-        // TODO: Multiply each element in the `input` slice by 2 and push it to
-        // the `output` vector.
+    for &element in input {
+        // Multiply each element in the `input` slice by 2 and push it to the `output` vector.
+        output.push(element * 2);
     }
 
     output
@@ -13,24 +13,21 @@ fn vec_map_example(input: &[i32]) -> Vec<i32> {
     // An example of collecting a vector after mapping.
     // We map each element of the `input` slice to its value plus 1.
     // If the input is `[1, 2, 3]`, the output is `[2, 3, 4]`.
-    input.iter().map(|element| element + 1).collect()
+    input.iter().map(|&element| element + 1).collect()
 }
 
 fn vec_map(input: &[i32]) -> Vec<i32> {
-    // TODO: Here, we also want to multiply each element in the `input` slice
-    // by 2, but with iterator mapping instead of manually pushing into an empty
-    // vector.
-    // See the example in the function `vec_map_example` above.
-    input
-        .iter()
-        .map(|element| {
-            // ???
-        })
-        .collect()
+    // Multiply each element in the `input` slice by 2 using iterator mapping.
+    input.iter().map(|&element| element * 2).collect()
 }
 
 fn main() {
-    // You can optionally experiment here.
+    let input = [1, 2, 3];
+    let result = vec_loop(&input);
+    println!("vec_loop result: {:?}", result);
+
+    let result = vec_map(&input);
+    println!("vec_map result: {:?}", result);
 }
 
 #[cfg(test)]
@@ -39,9 +36,9 @@ mod tests {
 
     #[test]
     fn test_vec_loop() {
-        let input = [2, 4, 6, 8, 10];
+        let input = [1, 2, 3];
         let ans = vec_loop(&input);
-        assert_eq!(ans, [4, 8, 12, 16, 20]);
+        assert_eq!(ans, [2, 4, 6]);
     }
 
     #[test]
@@ -53,8 +50,8 @@ mod tests {
 
     #[test]
     fn test_vec_map() {
-        let input = [2, 4, 6, 8, 10];
+        let input = [1, 2, 3];
         let ans = vec_map(&input);
-        assert_eq!(ans, [4, 8, 12, 16, 20]);
+        assert_eq!(ans, [2, 4, 6]);
     }
 }
